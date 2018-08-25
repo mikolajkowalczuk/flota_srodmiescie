fb3f8ef5f5f46222a22ac934bc99483d980375dd
Mikołaj Kowalczuk <sinice.entertainment.gmail.com>
2018-08-25 16:13:31 +0200

    commit

---
+++ main.js
@@ -0,0 +1,41 @@
+
+var slider={
+    jeden:function(){           
+      document.getElementById("image").src="1.jpg";
+        
+    },
+    dwa:function(){       
+        document.getElementById("image").src="2.jpg";
+
+    },
+    trzy:function(){
+       document.getElementById("image").src="3.jpg";
+    },
+    cztery:function(){
+        document.getElementById("image").src="4.jpg";
+     },
+     pięć:function(){
+        document.getElementById("image").src="5.jpg";
+     },
+     sześć:function(){
+        document.getElementById("image").src="6.jpg";
+     },
+     siedem:function(){
+        document.getElementById("image").src="7.jpg";
+     },
+     osiem:function(){
+        document.getElementById("image").src="8.jpg";
+     },
+     dziewięć:function(){
+        document.getElementById("image").src="9.jpg";
+     },
+     dziesięć:function(){
+        document.getElementById("image").src="10.jpg";
+     },
+    losowo:function(){
+        x=Math.floor(Math.random() * 10) + 1;
+        document.getElementById("image").src=x+".jpg";
+    },
+}
+
+
